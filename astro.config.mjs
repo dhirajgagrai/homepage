de58914f05507b6660266bbf7169efe953e5b53c
Dhiraj Gagrai <dhirajgagrai@gmail.com>
2025-02-17 17:38:56 +0530

    change: link for redirection of linux-kernel-dev-setup post

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   output: 'server',
   adapter: vercel(),
   redirects: {
-    '/linux-kernel-dev': {
+    '/linux-kernel-dev-setup': {
       status: 302,
       destination: '/posts/linux-kernel-dev-setup',
     },
