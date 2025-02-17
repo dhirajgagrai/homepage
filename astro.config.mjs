00a7aef39ceaa1fcbcd1b6abac638674685ff56d
Dhiraj Gagrai <dhirajgagrai@gmail.com>
2025-02-17 16:06:56 +0530

    change: output to ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,18 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://gagrai.com',
-    integrations: [sitemap()],
-    output: 'static'
+  site: 'https://gagrai.com',
+  integrations: [sitemap()],
+  output: 'server',
+  adapter: vercel(),
+  redirects: {
+    '/linux-kernel-dev': {
+      status: 302,
+      destination: '/posts/linux-kernel-dev-setup',
+    },
+  },
 });
